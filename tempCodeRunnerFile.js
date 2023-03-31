e0c0f57b8f7372b59362098ba662a8c0a1515c96
hyunwoomemo <archihw94@gmail.com>
2023-03-31 10:54:24 +0900

    apply .giignore

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const array = ['0', 1, 2, '0', '0', 3]
-
-const filterArr = array.reduce(
-  (unique, item) => (unique.includes(item) ? unique : [...unique, item]),
-  [],
-)
-
-console.log(filterArr);
